524ae5044d332f12002af9a361a13d716d5bff5d
Tyler Pearce <tyler@stuffedmotion.ca>
2020-02-12 09:38:15 -0500

    fix @shopify/theme-cart no longer exporting cookieEnabled

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,15 +1,15 @@
 import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
 
-import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
-import {cookiesEnabled} from '@shopify/theme-cart';
+import { focusHash, bindInPageLinks } from '@shopify/theme-a11y';
 
 // Common a11y fixes
 focusHash();
 bindInPageLinks();
 
 // Apply a specific class to the html element for browser support of cookies.
-if (cookiesEnabled()) {
+// @shopify/theme-cart no longer exports 'cookieEnabled'
+if (window.navigator.cookieEnabled) {
   document.documentElement.className = document.documentElement.className.replace(
     'supports-no-cookies',
     'supports-cookies',
